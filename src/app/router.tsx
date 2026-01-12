188041ed861bce612f30ee13a75a385736ce2bae
fogelt <fogeltt@gmail.com>
2026-01-12 03:17:13 +0100

    fixed config

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -43,9 +43,7 @@ export const createAppRouter = (queryClient: QueryClient) =>
         },
       ],
     }
-  ], {
-    basename: '/',
-  });
+  ]);
 
 export const AppRouter = () => {
   const queryClient = useQueryClient();
